2b2bad31bb9ef8573e0613ac70ef0791f0b9c945
mikaeltreppo <mikaeltreppo@outlook.com>
2023-02-28 15:11:14 +0100

    aggiornamenti

--- js/main.js
+++ js/main.js
@@ -7,17 +7,41 @@ const imageList = [
 ]
 console.log(imageList)
 
-// entro in html su imgSpace
-
+// entro in html su imgSpace e button
+let buttonNextDom = document.getElementById("next")
+let buttonPrevDom = document.getElementById("prev")
 let imgSpaceDom = document.querySelector(".imgSpace");
 
 let slider = "";
 // creo un creatore di img su js
-for( let i = 1; i < imageList.length +1 ; i++){
-     let sliderDone = `<img class="image" src="img/0${i}.webp" alt="img">`;
-     slider += sliderDone;
+for (let i = 1; i < imageList.length + 1; i++) {
+    let sliderDone = `<img class="image" src="img/0${i}.webp" alt="img">`;
+    slider += sliderDone;
 }
 // stampo le img su html
 imgSpaceDom.innerHTML = slider;
-//
+//creo array di classe e creo variabile con numeraione per incrementare in for
+const callImg = document.getElementsByClassName("image");
+
+let start = 0;
+
+callImg[start].classList.add("show");
+
+buttonNextDom.addEventListener('click', function () {
+
+    if (start < imageList.length - 1) {
+        callImg[start].classList.remove("show");
+        start++;
+        callImg[start].classList.add("show");
+
+    }
+   
+})
+
+
+
+
+//creo un add event su button
+
+
 
